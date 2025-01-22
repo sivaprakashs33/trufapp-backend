d9174a38c5462bfb4fab1f87224a4643cf66b86d
sivaprakash33 <sivaprakashkumar33@gmail.com>
2025-01-22 17:23:32 +0530

    add console

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@
 const connect =  await mongoose.connect(process.env.MONGODB_URL);
          console.log("connection to mongodb successfully")
      }catch(e){
+        console.log(e)
          process.exit(1)
      }
  }
